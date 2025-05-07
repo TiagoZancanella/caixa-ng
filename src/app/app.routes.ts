c8f57f69bb922465756d19a41bc9fe7c885a720b
Tiago Bazzi Zancanella <tiagozancanella17@gmail.com>
2025-05-07 16:14:30 -0300

    adicionado cadastro transação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { CadastroProdutoComponent } from './components/produtos/cadastro-produto/cadastro-produto.component';
 import { CadastroReceitaComponent } from './components/receitas/cadastro-receita/cadastro-receita.component';
 import { ClienteComponent } from './components/clientes/cliente/cliente.component';
+import { CadastroTransacaoComponent } from './components/cadastro-transacao/cadastro-transacao.component';
+
 
 export const routes: Routes = [
     {path: "produtos", component: CadastroProdutoComponent},
     {path: "receitas", component: CadastroReceitaComponent},
-    {path: "clientes", component: ClienteComponent}
+    {path: "clientes", component: ClienteComponent},
+    {path: "transacao.model", component: CadastroTransacaoComponent},
 ];
